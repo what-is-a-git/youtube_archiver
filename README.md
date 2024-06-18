14a5a60cd2d6bbe787da45c6ada856e1361debd8
what-is-a-git <138817584+what-is-a-git@users.noreply.github.com>
2024-06-17 20:04:41 -0400

    program actually works good now

---
+++ README.md
@@ -0,0 +1,11 @@
+# youtube_archiver
+
+funny rust program that downloads all the information
+about a youtube video and the video itself
+
+## apis used
+
+- cobalt
+    * the actual video downloading stuff
+- yt.lemnoslife.com
+    * youtube data api v3 without a key (accessible for all & free)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs::File, io::Write};
-
+use std::{fs::{create_dir_all, File}, io::Write};
 use serde::{Serialize, Deserialize};
+use colored::*;
+use clap::Parser;
 
 #[allow(non_snake_case)] // needed for cobalt api
 #[derive(Serialize)]
@@ -30,40 +31,261 @@ struct ResponseBody {
     // audio: Option<String>,
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long)]
+    url: String,
+
+    #[arg(short, long)]
+    dir: String,
+}
+
 fn main() {
+    let args = Arguments::parse();
+    let directory = args.dir;
+    let url = args.url;
+
+    create_dir_all(&directory).unwrap();
+
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap()
         .block_on(async {
-            request_video(DownloadParameters {
-                url: String::from("https://youtu.be/agTY4O0qsrc"),
+            request_metadata(MetadataParameters {
+                url: &url,
+                dir: &directory,
+            }).await;
+
+            request_video(VideoParameters {
+                url: &url,
                 video_codec: String::from("h264"),
-                filename: String::from("source_h264.mp4"),
+                filename: format!("{}/source_h264.mp4", &directory),
             }).await;
         });
 }
 
-async fn request_video(params: DownloadParameters) {
-    let result = download_video(params).await;
+#[derive(Debug)]
+struct MetadataParameters<'a> {
+    url: &'a String,
+    dir: &'a String,
+}
 
-    if result.is_ok() {
-        println!("Successfully downloaded requested video!");
-    } else {
+async fn request_metadata(params: MetadataParameters<'_>) {
+    let id = get_id_from_url(params.url);
+    let meta_result = download_metadata(format!("https://yt.lemnoslife.com/noKey/videos?part=snippet&id={id}")).await;
+
+    if meta_result.is_err() {
+        let error = meta_result.err().unwrap();
+        println!("{} {}", " FAILURE ".on_red(), error);
+        return;
+    }
+
+    let meta = meta_result.unwrap();
+    assert!(meta.items.len() > 0, "YouTube API Response had no metadata items! Maybe try again later?");
+
+    let write_result = write_metadata(&meta.items[0], params.dir);
+    if write_result.is_err() {
+        let error = write_result.err().unwrap();
+        println!("{} {}", " FAILURE ".on_red(), error);
+        return;
+    }
+
+    let thumbnails = &meta.items[0].snippet.thumbnails;
+    request_thumbnail(ThumbnailParameters {
+        url: &thumbnails.default.url,
+        filename: format!("{}/thumb_default.jpg", params.dir),
+    }).await;
+    request_thumbnail(ThumbnailParameters {
+        url: &thumbnails.medium.url,
+        filename: format!("{}/thumb_medium.jpg", params.dir),
+    }).await;
+    request_thumbnail(ThumbnailParameters {
+        url: &thumbnails.high.url,
+        filename: format!("{}/thumb_high.jpg", params.dir),
+    }).await;
+    request_thumbnail(ThumbnailParameters {
+        url: &thumbnails.standard.url,
+        filename: format!("{}/thumb_standard.jpg", params.dir),
+    }).await;
+    request_thumbnail(ThumbnailParameters {
+        url: &thumbnails.maxres.url,
+        filename: format!("{}/thumb_maxres.jpg", params.dir),
+    }).await;
+}
+
+#[derive(Serialize)]
+struct ArchivedMetadata {
+    id: String,
+    title: String,
+    description: String,
+    creator: String,
+    publish_date: String,
+    tags: Vec<String>,
+}
+
+fn write_metadata(input: &ItemResponse, dir: &String) -> Result<(), String> {
+    let mut tags: Vec<String> = Vec::new();
+
+    if input.snippet.tags.is_some() {
+        tags = input.snippet.tags.as_ref().unwrap().clone();
+    }
+
+    let output_data = ArchivedMetadata {
+        title: input.snippet.title.clone(),
+        description: input.snippet.description.clone(),
+        creator: input.snippet.channelTitle.clone(),
+        publish_date: input.snippet.publishedAt.clone(),
+        tags: tags,
+        id: input.id.clone(),
+    };
+    let output_filename = format!("{dir}/meta.json");
+    let mut output_file = File::create(&output_filename).unwrap();
+    let output_contents = serde_json::to_string_pretty(&output_data).unwrap();
+    let write_result = output_file.write_all(output_contents.as_bytes());
+    
+    if !write_result.is_ok() {
+        return Err(format!("Couldn't write to file {}... Error: {:?}", &output_filename, write_result.err()));
+    }
+
+    println!("{} Wrote to requested file {} successfully!", " SUCCESS ".on_green(), &output_filename);
+    Ok(())
+}
+
+#[allow(non_snake_case)] // needed for youtube api
+#[derive(Debug, Deserialize)]
+struct ThumbnailResponse {
+    url: String,
+}
+
+#[allow(non_snake_case)] // needed for youtube api
+#[derive(Debug, Deserialize)]
+struct ThumbnailsResponse {
+    default: ThumbnailResponse,
+    medium: ThumbnailResponse,
+    high: ThumbnailResponse,
+    standard: ThumbnailResponse,
+    maxres: ThumbnailResponse,
+}
+
+#[allow(non_snake_case)] // needed for youtube api
+#[derive(Debug, Deserialize)]
+struct SnippetResponse {
+    publishedAt: String,
+    title: String,
+    description: String,
+    thumbnails: ThumbnailsResponse,
+    channelTitle: String,
+    tags: Option<Vec<String>>,
+}
+
+#[allow(non_snake_case)] // needed for youtube api
+#[derive(Debug, Deserialize)]
+struct ItemResponse {
+    id: String,
+    snippet: SnippetResponse,
+}
+
+#[allow(non_snake_case)] // needed for youtube api
+#[derive(Debug, Deserialize)]
+struct YouTubeResponse {
+    items: Vec<ItemResponse>,
+}
+
+async fn download_metadata(url: String) -> Result<YouTubeResponse, String> {
+    println!("{} Requesting metadata at this url: {}", " REQUEST ".on_cyan(), &url);
+    let client = reqwest::Client::new();
+    let result = client.get(url)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .send().await;
+
+    if result.is_err() {
         let error = result.err().unwrap();
-        println!("Error while downloading video!\nError: {error}");
+        return Err(format!("Error while getting metadata! Error: {error}"));
+    }
+
+    let response = result.unwrap().json::<YouTubeResponse>().await;
+    if response.is_err() {
+        let error = response.err().unwrap();
+        return Err(format!("Error while parsing metadata! Error: {error}"));
+    }
+
+    Ok(response.unwrap())
+}
+
+#[derive(Debug)]
+struct ThumbnailParameters<'a> {
+    url: &'a String,
+    filename: String,
+}
+
+async fn request_thumbnail(params: ThumbnailParameters<'_>) {
+    println!("{} Downloading thumbnail with these parameters: {params:?}", " REQUEST ".on_cyan());
+    let result = download_thumbnail(params).await;
+
+    if result.is_err() {
+        let error = result.err().unwrap();
+        println!("{} Error while downloading thumbnail! Error: {error}", " FAILURE ".on_red());
+        return;
     }
 }
 
-struct DownloadParameters {
-    url: String,
+async fn download_thumbnail(params: ThumbnailParameters<'_>) -> Result<(), String> {
+    let client = reqwest::Client::new();
+    let result = client.get(params.url)
+        .send().await;
+
+    if result.is_err() {
+        return Err(format!("Failed to request {}! Error: {:?}", params.url, result.err().unwrap()));
+    }
+
+    let response = result.unwrap();
+    let contents = response.bytes().await.unwrap();
+    let mut output_file = File::create(params.filename.clone()).unwrap();
+    let write_result = output_file.write_all(&contents);
+    
+    if !write_result.is_ok() {
+        return Err(format!("Couldn't write to file {}... Error: {:?}", params.filename, write_result.err()));
+    }
+
+    println!("{} Wrote to requested file {} successfully!", " SUCCESS ".on_green(), params.filename);
+    Ok(())
+}
+
+fn get_id_from_url(url: &String) -> String {
+    assert!(url.find("youtu").is_some(), "Make sure to provide a valid YouTube URL!");
+    let clean_url = url.split_at(url.find("youtu").unwrap()).1;
+
+    if clean_url.starts_with("youtube.com/watch?v=") { // youtube.com/watch?v=id
+        String::from(clean_url.split_at(clean_url.find("=").unwrap() + 1).1)
+    } else { // youtu.be/id
+        String::from(clean_url.split_at(clean_url.find("/").unwrap() + 1).1)
+    }
+}
+
+#[derive(Debug)]
+struct VideoParameters<'a> {
+    url: &'a String,
     video_codec: String,
     filename: String,
 }
 
-async fn download_video(params: DownloadParameters) -> Result<(), String> {
+async fn request_video(params: VideoParameters<'_>) {
+    println!("{} Downloading video with these parameters: {params:?}", " REQUEST ".on_cyan());
+    let result = download_video(params).await;
+
+    if result.is_err() {
+        let error = result.err().unwrap();
+        println!("{} Error while downloading video! Error: {error}", " FAILURE ".on_red());
+        return;
+    }
+}
+
+async fn download_video(params: VideoParameters<'_>) -> Result<(), String> {
     let client = reqwest::Client::new();
-    let result = client.post("https://api.cobalt.tools/api/json")
+    let initial_result = client.post("https://api.cobalt.tools/api/json")
         .header("Accept", "application/json")
         .header("Content-Type", "application/json")
         .json(&RequestBody {
@@ -71,7 +293,7 @@ async fn download_video(params: DownloadParameters) -> Result<(), String> {
             vCodec: params.video_codec.as_str(),
             vQuality: "max",
             aFormat: "best",
-            filenamePattern: "pretty",
+            filenamePattern: "classic",
             isAudioOnly: false,
             isTTFullAudio: false,
             isAudioMuted: false,
@@ -82,46 +304,42 @@ async fn download_video(params: DownloadParameters) -> Result<(), String> {
         })
         .send().await;
 
-    if result.is_ok() {
-        println!("Got a successful response from api.cobalt.tools/api/json!");
+    if !initial_result.is_ok() {
+        return Err(String::from("Got an error while posting to api.cobalt.tools/api/json! Maybe check your internet connection?"));
+    }
 
-        let response = result.unwrap().json::<ResponseBody>().await.unwrap();
-        let status = response.status;
-        match status.as_str() {
-            "error" => {
-                let text = response.text.unwrap();
-                println!("Got an error posting to the api!\nMessage: {text}");
-                return Err(text);
-            },
-            "rate-limit" => {
-                println!("Reached the rate-limit on cobalt's api! Maybe try again later.");
-                return Err(String::from("Rate limited."));
-            },
-            "stream" => {
-                println!("Got a valid video stream! Getting file now.");
-                let get_url = response.url.unwrap();
-                let get_response = client.get(get_url)
-                    .header("Accept", "application/json")
-                    .header("Content-Type", "application/json")
-                    .send().await.unwrap();
-                let video_contents = get_response.bytes().await.unwrap();
-                let mut output_file = File::create(params.filename.clone()).unwrap();
-                let write_result = output_file.write_all(&video_contents);
-                
-                if write_result.is_ok() {
-                    println!("Wrote requested to file successfully at {}!", params.filename);
-                } else {
-                    println!("Couldn't write to file {}... Error: {:?}", params.filename, write_result.err());
-                }
-            },
-            _ => {
-                println!("Unimplemented status recieved!\nStatus: {status}");
-                return Err(String::from("Unimplemented status."));
-            },
-        }
-    } else {
-        println!("Got an error while posting to api.cobalt.tools/api/json! Maybe check your internet connection?");
-        return Err(String::from("Error posting to api."));
+    println!("{} Got response from the cobalt api!", " SUCCESS ".on_green());
+
+    let initial_response = initial_result.unwrap().json::<ResponseBody>().await.unwrap();
+    let status = initial_response.status;
+    match status.as_str() {
+        "error" => {
+            let text = initial_response.text.unwrap();
+            return Err(format!("Got an error posting to the cobalt api! Message: {text}"));
+        },
+        "rate-limit" => {
+            return Err(String::from("Rate-limited from the cobalt api."));
+        },
+        "stream" => {
+            println!("{} Got a valid video stream! Now getting file.", " SUCCESS ".on_green());
+            let get_url = initial_response.url.unwrap();
+            let get_response = client.get(get_url)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .send().await.unwrap();
+            let video_contents = get_response.bytes().await.unwrap();
+            let mut output_file = File::create(params.filename.clone()).unwrap();
+            let write_result = output_file.write_all(&video_contents);
+            
+            if !write_result.is_ok() {
+                return Err(format!("Couldn't write to file {}. Error: {:?}", params.filename, write_result.err().unwrap()));
+            }
+
+            println!("{} Wrote to requested file {} successfully!", " SUCCESS ".on_green(), params.filename);
+        },
+        _ => {
+            return Err(format!("No implementation for status {status}."));
+        },
     }
 
     Ok(())
