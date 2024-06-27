75726f219f4d9283068d258ef799391545d36b0d
what-is-a-git <138817584+what-is-a-git@users.noreply.github.com>
2024-06-27 14:49:27 -0400

    Force printing log type as black text to improve readability in some cases

--- src/log.rs
+++ src/log.rs
@@ -1,13 +1,13 @@
 use colored::*;
 
 pub fn request(input: String) {
-    println!("{} {input}", " REQUEST ".on_cyan());
+    println!("{} {input}", " REQUEST ".black().on_cyan());
 }
 
 pub fn success(input: String) {
-    println!("{} {input}", " SUCCESS ".on_green());
+    println!("{} {input}", " SUCCESS ".black().on_green());
 }
 
 pub fn failure(input: String) {
-    println!("{} {input}", " FAILURE ".on_red());
+    println!("{} {input}", " FAILURE ".black().on_red());
 }
